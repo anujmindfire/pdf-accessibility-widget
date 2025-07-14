d2d44f1ae38934b52c4e8183b1c65b390d3be542
Anuj Singh <anuj.s@mindfiresolutions.com>
2025-07-14 18:37:00 +0530

    feat: added pdf

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App" style={{ margin: 0, padding: 0, height: '100vh', overflow: 'auto' }}>
-      <div id="pdf-viewer" style={{ width: '100%', paddingBottom: '100px' }}></div>
-      <accessibility-widget></accessibility-widget>
-    </div>
+    <div/>
   );
 }
 
